dd4ce94abb9697f67fc1f62b85a0893892b69100
AisyahNadhilah <aisyahnadhilah123@gmail.com>
2024-11-09 16:38:35 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<html>
+
+<body>
+
+      @include('partials.header')
+      <p> about page </p>
+
+      <image src=" {{ asset ('assets/Picture.jpeg') }}"
+      alt="aisyah logo" style="width: 100; height 150;">
+
+      <p> Nice to meet you </p> 
+      <p> I'm a software engineering student </p>
+      
+      @include('partials.footer')
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<html>
+
+<body>
+
+    @include('partials.header')
+
+    <h2> Home </h2>
+
+    @include('partials.footer')
+
+</body>
+
+</html>
+
